7c28b9b98d253a01dbd823bece6a49bc6e5fe89c
Guilherme Fusuma <fuskenji@gmail.com>
2025-08-27 14:48:33 -0300

    Criados FBV e CBV para listagem de herois

--- heroes/views.py
+++ heroes/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import DimHero
+from django.views.generic import ListView
 
 def lista_herois(request):
-    herois = DimHero.objects.all()  # busca todos os heróis do banco
-    return render(request, "heroes/lista_herois.html", {"herois": herois})
+    heroes = DimHero.objects.all()  # busca todos os heróis do banco
+    return render(request, "heroes/lista_herois.html", {"heroes": heroes})
+
+#CBV - CLASS-BASED-VIEW view baseada em classe com generic ListV    iew
+class HeroListView(ListView):
+    model = DimHero
+    template_name = "heroes/lista_herois.html"
+    context_object_name = "heroes"
