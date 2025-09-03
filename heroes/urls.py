b5eb0f3dd49834ce609d7b6de79ae62f75f94066
Guilherme Fusuma <fuskenji@gmail.com>
2025-09-03 14:08:22 -0300

    Adicionado maioria dos templates

--- heroes/urls.py
+++ heroes/urls.py
@@ -3,5 +3,5 @@ from .views import lista_herois, HeroListView
 
 urlpatterns = [
     path('lista/', lista_herois, name='lista_herois'),
-    path('cbv-lista', HeroListView.as_view(), name="cbv_lista_herois"),
+    path('cbv-lista/', HeroListView.as_view(), name='cbv_lista_herois'),
 ]
--- heroes/views.py
+++ heroes/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from .models import DimHero
 from django.views.generic import ListView
+from .models import DimHero
 
+
+#FBV - function-based view - view baseada em função
 def lista_herois(request):
-    heroes = DimHero.objects.all()  # busca todos os heróis do banco
-    return render(request, "heroes/lista_herois.html", {"heroes": heroes})
+    herois = DimHero.objects.all()  # busca todos os heróis do banco
+    return render(request, "heroes/lista_herois.html", {"herois": herois})
 
-#CBV - CLASS-BASED-VIEW view baseada em classe com generic ListV    iew
+#CBV - CLASS-BASED VIEW - VIEW BASEADA EM CLASSE COM GENERIC LIST VIEW
 class HeroListView(ListView):
     model = DimHero
     template_name = "heroes/lista_herois.html"
-    context_object_name = "heroes"
+    context_object_name = "herois"
