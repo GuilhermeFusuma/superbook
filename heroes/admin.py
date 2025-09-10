fc168706c532477df37f245abbed92cc4206ca08
Guilherme Fusuma <fuskenji@gmail.com>
2025-09-10 14:21:41 -0300

    Adicionado Hero ao admin

--- heroes/admin.py
+++ heroes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Hero
 
 # Register your models here.
+class HeroAdmin(admin.ModelAdmin):
+    list_display = ['codinome', 'nome_real', 'poder_principal', 'cidade', 'criado_em']
+    list_filter = ['cidade']
+    search_fields = ['codinome', 'nome_real', 'cidade']
+
+
+admin.site.register(Hero)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Post, Like, Comment
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    fields = [
+        "autor",
+        "mensagem",
+        "imagem",
+        "data_criacao"
+    ]
+
+class LikeAdmin(admin.ModelAdmin):
+    fields = [
+        "hero",
+        "post",
+        "data_criacao"
+    ]
+
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
