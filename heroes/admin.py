763c4ca620bf023dbf5e6bca2ead569f6cd5140d
Guilherme Fusuma <fuskenji@gmail.com>
2025-09-10 14:27:49 -0300

    seções criadas nos dados do Hero no painel de admin

--- heroes/admin.py
+++ heroes/admin.py
@@ -4,6 +4,19 @@ from .models import Hero
 # Register your models here.
 @admin.register(Hero)
 class HeroAdmin(admin.ModelAdmin):
-    list_display = ['codinome', 'nome_real', 'poder', 'cidade', 'data_criacao']
-    list_filter = ['cidade']
-    search_fields = ['codinome', 'nome_real', 'cidade']
+    list_display = ['codinome', 'nome_real', 'poder', 'cidade', 'data_criacao'] # campos exibidos
+    list_filter = ['cidade'] # campos para serem filtrados
+    search_fields = ['codinome', 'nome_real', 'cidade'] # campos que podem ser pesquisados
+
+    fieldsets = ( # divide os campos em várias seções
+        ('Identidade Secreta', {
+            'fields': ('codinome', 'nome_real')
+        }),
+        ('Informações Gerais', {
+            'fields': ('poder', 'cidade', 'historia')
+        }),
+        ('Dados de Registro', {
+            'fields': ('data_criacao',)
+        }),
+    )
+    readonly_fields = ['data_criacao']
