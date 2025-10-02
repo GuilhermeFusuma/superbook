7b5b4db1da75f639da17abc25131be9ce3f91749
Guilherme Fusuma <fuskenji@gmail.com>
2025-10-02 16:15:03 -0300

    Feito crud e outras telas de vilões

--- villains/urls.py
+++ villains/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('lista/', views.VillainListView.as_view(), name='lista_viloes'),
+    path("novo/", views.VillainCreateView.as_view(), name="novo_vilao"),
+    path("<int:pk>/editar/", views.VillainUpdateView.as_view(), name="editar_vilao"),
+    path("<int:pk>/excluir/", views.VillainDeleteView.as_view(), name="excluir_vilao"),
+    path("<int:pk>/", views.VillainDetailView.as_view(), name="detalhe_vilao"),
 ]
 
--- villains/views.py
+++ villains/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Villain
+from .forms import VillainForm
 
 class VillainListView(ListView):
     model = Villain
-    template_name = 'villains/lista_viloes.html'
-    context_object_name = 'viloes'
+    template_name = 'villains/lista_villains.html'
+    context_object_name = 'villains'
 
+class VillainDetailView(DetailView):
+    model = Villain
+    template_name = "villains/detalhe_Villain.html"
+    context_object_name = "villain"
+
+class VillainCreateView(CreateView):
+    model = Villain
+    form_class = VillainForm
+    template_name = "villains/form_Villain.html"
+    success_url = reverse_lazy("lista_viloes")
+
+class VillainUpdateView(UpdateView):
+    model = Villain
+    form_class = VillainForm
+    template_name = "villains/form_Villain.html"
+    success_url = reverse_lazy("lista_viloes")
+
+class VillainDeleteView(DeleteView):
+    model = Villain
+    template_name = "villains/confirmar_exclusao.html"
+    success_url = reverse_lazy("lista_viloes")
